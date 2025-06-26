d0cf96c1b93f4a2d5ec01b026d74c67424379147
sabrina <peiching913@gmail.com>
2025-06-26 10:37:10 +0800

    feat: add reserve pages

--- timesnap/urls.py
+++ timesnap/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pages.urls')),
+    path('', include('pages.urls', namespace='pages')),
+    path('reserve/', include('reserve.urls', namespace='reserve' )),
 ]
