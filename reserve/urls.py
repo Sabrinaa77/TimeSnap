f5b32d0b917bd2b588dcb95f375da79997de70cb
sabrina <peiching913@gmail.com>
2025-06-27 11:39:43 +0800

    feat: reserve function

--- reserve/urls.py
+++ reserve/urls.py
@@ -1,9 +1,16 @@
 from . import views
 from django.urls import path
+from .views import ReserveListAPIView
+from reserve.views import calendar_page
 
 app_name = 'reserve'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('new/', views.new, name='new'),
+    path('show/<uuid:id>/', views.show, name='show'),
+    path('edit/<uuid:id>/', views.edit, name='edit'),
+    path('api/reserve/', ReserveListAPIView.as_view(), name='reservae-list'),
+    path('calendar/', calendar_page, name='calendar-page'),
 ]
 
