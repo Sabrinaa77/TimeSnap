a024b03a901f9cd60eb33ee90dbdbf553e404a1f
sabrina <peiching913@gmail.com>
2025-06-27 16:46:08 +0800

    feat: add postgresql setting

--- timesnap/settings.py
+++ timesnap/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -80,10 +80,8 @@ WSGI_APPLICATION = 'timesnap.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 自動解析 DATABASE_URL
+    'default': env.db()
 }
 
 
