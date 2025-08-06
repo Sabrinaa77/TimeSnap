668681baae7378469ab53fa42b3a2f7b60025e68
sabrina <peiching913@gmail.com>
2025-08-06 17:34:31 +0800

    feat: add signup link to navigation menu

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,23 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+# 個人資料
 @login_required
 def profile(request):
     return render(request, 'accounts/profile.html')
+
+# 註冊
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # 註冊成功後自動登入
+            return redirect('profile')  # 註冊成功後導向個人頁面
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html', {'form': form})
